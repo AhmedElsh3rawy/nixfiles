b701827e7948eb5c20ca618595eaba3eb3db580a
Ahmed <ahmedelsharawy117@gmail.com>
2024-07-12 11:45:45 +0300

    modify alacritty.nix

--- system/alacritty.nix
+++ system/alacritty.nix
@@ -3,15 +3,15 @@
 {
   programs.alacritty = {
     enable = true;
-    window.opacity = 0.7;
-    scrolling.history = 1000;
-    font = {
-      normal = {
-        family = "JetBrains Mono Nerd Font";
-        style = "Regular";
+    settings = {
+      font = {
+        normal = {
+          family = "JetBrains Mono Nerd Font";
+          style = "Regular";
+        };
+        size = 10;
       };
-
-      size = 10;
     };
+
   };
 }
