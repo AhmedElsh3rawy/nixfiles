dc51317de22a452c363f7469b42aa9aa25fe2978
Ahmed <ahmedelsharawy117@gmail.com>
2024-07-13 21:32:16 +0300

    change alacritty opacity

--- system/alacritty.nix
+++ system/alacritty.nix
@@ -4,12 +4,18 @@
   programs.alacritty = {
     enable = true;
     settings = {
+      env = {
+        "TERM" = "xterm-256color";
+      };
+
+      window.opacity = 1;
+
       font = {
         normal = {
           family = "JetBrains Mono Nerd Font";
           style = "Regular";
         };
-        size = 10;
+        size = 9;
       };
     };
 
